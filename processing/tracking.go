3e6b8bf906ad68fa2a51f149a6b1d840d422886f
Dorian Stoll <dorian.stoll@tmsp.io>
2020-05-29 20:28:21 +0200

    Add some documentation to the finger tracking
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- processing/tracking.go
+++ processing/tracking.go
@@ -50,6 +50,11 @@ func TrackFingers(points []TouchPoint, count int) {
 		return
 	}
 
+	/*
+	 * For every current point, calculate the distance to all points from
+	 * the last cycle. Then use these distances to create a sorted list
+	 * of their indices, going from nearest to furthest away.
+	 */
 	for i := 0; i < len(points); i++ {
 		for j := 0; j < len(points); j++ {
 			indices[i][j] = j
@@ -68,11 +73,22 @@ func TrackFingers(points []TouchPoint, count int) {
 		})
 	}
 
+	/*
+	 * Choose the index of the nearest point from the previous cycle
+	 */
 	for i := 0; i < count; i++ {
 		points[i].Index = last[indices[i][0]].Index
 	}
 
-	// Find duplicates
+	/*
+	 * The above selection will definitly lead to duplicates. For example,
+	 * a new contact will always get the index 0, because that is the
+	 * smallest distance that will be calculated (MAX_INT - len(last) + 0)
+	 *
+	 * To fix this we will iterate over the points, searching and fixing
+	 * duplicates until every point has an unique index (or -1, because
+	 * that is handled seperately).
+	 */
 	for j := 1; j < len(points); j++ {
 		duplicates := 0
 
@@ -83,6 +99,11 @@ func TrackFingers(points []TouchPoint, count int) {
 				continue
 			}
 
+			/*
+			 * Point A is a duplicate of point B if they have the
+			 * same index, and B is closer to the point from the
+			 * last cycle with the same index.
+			 */
 			for k := 0; k < count; k++ {
 				if k == i {
 					continue
@@ -104,14 +125,33 @@ func TrackFingers(points []TouchPoint, count int) {
 				continue
 			}
 
+			/*
+			 * If we change the index now, the points that are
+			 * checked after this one will think they are
+			 * duplicates. We set the index to -2 and fix it up
+			 * after all other points have been checked for this
+			 * iteration.
+			 */
 			points[i].Index = -2
 			duplicates++
 		}
 
+		/*
+		 * If we haven't found any duplicates we don't need to
+		 * continue searching for them.
+		 */
 		if duplicates == 0 {
 			break
 		}
 
+		/*
+		 * Update the index for all points with index -2 (duplicates)
+		 *
+		 * We started by using the index of the nearest point from the
+		 * previous cycle. Since that resulted in a duplicate we use
+		 * the next-nearest point (incremented the index). We will
+		 * continue to do that until there are no duplicates anymore.
+		 */
 		for i := 0; i < len(points); i++ {
 			if points[i].Index != -2 {
 				continue
@@ -126,7 +166,16 @@ func TrackFingers(points []TouchPoint, count int) {
 		}
 	}
 
-	// Assign new indices to those points who have index -1
+	/*
+	 * If by now one of the found points still has the Index -1, it is a
+	 * new point, so we need to find a free index for it to use.
+	 *
+	 * This is not really complicated but the code is not that simple.
+	 * We iterate over all points to find the one with index -1. Then
+	 * we go through every possible index to see if it is already used by
+	 * other points. If we cannot find a point using the index we assign
+	 * it and continue to the next point.
+	 */
 	for i := 0; i < count; i++ {
 		if points[i].Index != -1 {
 			continue
@@ -157,5 +206,13 @@ func TrackFingers(points []TouchPoint, count int) {
 		}
 	}
 
+	/*
+	 * Finally, we need to save the current list of points to use them in
+	 * the next cycle of course.
+	 *
+	 * Since the points list is a cached array, we cannot just assign it,
+	 * because then "points" and "last" would be identical. Instead we
+	 * need to go through them and copy over every element.
+	 */
 	SavePoints(points)
 }
