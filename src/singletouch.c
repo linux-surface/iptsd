7f416d6c92c4bebba79b5a7d1ad6e58d26195b01
Dorian Stoll <dorian.stoll@tmsp.io>
2020-11-26 13:35:54 +0100

    Implement the additional events also for singletouch
    This way the singletouch data is sent to userspace the same way as the
    multitouch data for older gens.
    
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- src/singletouch.c
+++ src/singletouch.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+#include <linux/input-event-codes.h>
 #include <linux/uinput.h>
 
 #include "context.h"
@@ -7,6 +8,35 @@
 #include "protocol.h"
 #include "singletouch.h"
 #include "utils.h"
+
+static void iptsd_singletouch_lift(int dev)
+{
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_SLOT, 0);
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_TRACKING_ID, -1);
+
+	iptsd_devices_emit(dev, EV_KEY, BTN_TOUCH, 0);
+}
+
+static void iptsd_singletouch_emit(int dev, struct ipts_singletouch_data *data)
+{
+	int x = (int)((double)data->x / 32767 * IPTS_MAX_X);
+	int y = (int)((double)data->y / 32767 * IPTS_MAX_Y);
+
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_SLOT, 0);
+
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_TRACKING_ID, 0);
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_POSITION_X, x);
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_POSITION_Y, y);
+
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_TOOL_TYPE, MT_TOOL_FINGER);
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_TOOL_X, x);
+	iptsd_devices_emit(dev, EV_ABS, ABS_MT_TOOL_Y, y);
+
+	iptsd_devices_emit(dev, EV_KEY, BTN_TOUCH, 1);
+	iptsd_devices_emit(dev, EV_ABS, ABS_X, x);
+	iptsd_devices_emit(dev, EV_ABS, ABS_Y, y);
+}
+
 
 int iptsd_singletouch_handle_input(struct iptsd_context *iptsd,
 		struct ipts_data *header)
@@ -16,18 +46,10 @@ int iptsd_singletouch_handle_input(struct iptsd_context *iptsd,
 	struct ipts_singletouch_data *data =
 		(struct ipts_singletouch_data *)&header->data[1];
 
-	int x = (int)((double)data->x / 32767 * IPTS_MAX_X);
-	int y = (int)((double)data->y / 32767 * IPTS_MAX_Y);
-
-	iptsd_devices_emit(touch.dev, EV_ABS, ABS_MT_SLOT, 0);
-
-	if (data->touch) {
-		iptsd_devices_emit(touch.dev, EV_ABS, ABS_MT_TRACKING_ID, 0);
-		iptsd_devices_emit(touch.dev, EV_ABS, ABS_MT_POSITION_X, x);
-		iptsd_devices_emit(touch.dev, EV_ABS, ABS_MT_POSITION_Y, y);
-	} else {
-		iptsd_devices_emit(touch.dev, EV_ABS, ABS_MT_TRACKING_ID, -1);
-	}
+	if (data->touch)
+		iptsd_singletouch_emit(touch.dev, data);
+	else
+		iptsd_singletouch_lift(touch.dev);
 
 	int ret = iptsd_devices_emit(touch.dev, EV_SYN, SYN_REPORT, 0);
 	if (ret < 0)
