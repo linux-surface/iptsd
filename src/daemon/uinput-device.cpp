a4596e5f1f23e04cfffb18ee72a44e248c8f8f46
Dorian Stoll <dorian.stoll@tmsp.io>
2021-03-05 13:18:19 +0100

    daemon: clang-tidy the uinput wrapper
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- src/daemon/uinput-device.cpp
+++ src/daemon/uinput-device.cpp
@@ -3,6 +3,7 @@
 #include "uinput-device.hpp"
 
 #include <common/cerror.hpp>
+#include <common/cwrap.hpp>
 #include <common/types.hpp>
 
 #include <cstring>
@@ -12,90 +13,86 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
-UinputDevice::UinputDevice(void)
+UinputDevice::UinputDevice()
 {
-	int ret = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+	int ret = iptsd::common::open("/dev/uinput", O_WRONLY | O_NONBLOCK);
 	if (ret == -1)
 		throw iptsd::common::cerror("Failed to open uinput device");
 
 	this->fd = ret;
 }
 
-UinputDevice::~UinputDevice(void)
+UinputDevice::~UinputDevice()
 {
-	ioctl(this->fd, UI_DEV_DESTROY, nullptr);
+	iptsd::common::ioctl(this->fd, UI_DEV_DESTROY);
 	close(this->fd);
 }
 
 void UinputDevice::set_evbit(i32 ev)
 {
-	int ret = ioctl(this->fd, UI_SET_EVBIT, (int)ev);
+	int ret = iptsd::common::ioctl(this->fd, UI_SET_EVBIT, (int)ev);
 	if (ret == -1)
 		throw iptsd::common::cerror("UI_SET_EVBIT failed");
 }
 
 void UinputDevice::set_keybit(i32 key)
 {
-	int ret = ioctl(this->fd, UI_SET_KEYBIT, (int)key);
+	int ret = iptsd::common::ioctl(this->fd, UI_SET_KEYBIT, (int)key);
 	if (ret == -1)
 		throw iptsd::common::cerror("UI_SET_KEYBIT failed");
 }
 
 void UinputDevice::set_propbit(i32 prop)
 {
-	int ret = ioctl(this->fd, UI_SET_PROPBIT, (int)prop);
+	int ret = iptsd::common::ioctl(this->fd, UI_SET_PROPBIT, (int)prop);
 	if (ret == -1)
 		throw iptsd::common::cerror("UI_SET_PROPBIT failed");
 }
 
 void UinputDevice::set_absinfo(u16 code, i32 min, i32 max, i32 res)
 {
-	struct uinput_abs_setup abs;
-
-	std::memset(&abs, 0, sizeof(abs));
+	struct uinput_abs_setup abs {};
 
 	abs.code = code;
 	abs.absinfo.minimum = min;
 	abs.absinfo.maximum = max;
 	abs.absinfo.resolution = res;
 
-	int ret = ioctl(this->fd, UI_ABS_SETUP, &abs);
+	int ret = iptsd::common::ioctl(this->fd, UI_ABS_SETUP, &abs);
 	if (ret == -1)
 		throw iptsd::common::cerror("UI_ABS_SETUP failed");
 }
 
-void UinputDevice::create(void)
+void UinputDevice::create()
 {
-	struct uinput_setup setup;
-
-	std::memset(&setup, 0, sizeof(setup));
+	struct uinput_setup setup {};
 
 	setup.id.bustype = BUS_VIRTUAL;
 	setup.id.vendor = this->vendor;
 	setup.id.product = this->product;
 	setup.id.version = this->version;
-	std::strncpy(setup.name, this->name.c_str(), this->name.length());
 
-	int ret = ioctl(this->fd, UI_DEV_SETUP, &setup);
+	// NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+	this->name.copy(setup.name, this->name.length(), 0);
+
+	int ret = iptsd::common::ioctl(this->fd, UI_DEV_SETUP, &setup);
 	if (ret == -1)
 		throw iptsd::common::cerror("UI_DEV_SETUP failed");
 
-	ret = ioctl(this->fd, UI_DEV_CREATE, nullptr);
+	ret = iptsd::common::ioctl(this->fd, UI_DEV_CREATE);
 	if (ret == -1)
 		throw iptsd::common::cerror("UI_DEV_CREATE failed");
 }
 
 void UinputDevice::emit(u16 type, u16 key, i32 value)
 {
-	struct input_event ie;
-
-	std::memset(&ie, 0, sizeof(ie));
+	struct input_event ie {};
 
 	ie.type = type;
 	ie.code = key;
 	ie.value = value;
 
-	int ret = write(this->fd, &ie, sizeof(ie));
+	ssize_t ret = write(this->fd, &ie, sizeof(ie));
 	if (ret == -1)
 		throw iptsd::common::cerror("Failed to write input event");
 }
--- src/daemon/uinput-device.hpp
+++ src/daemon/uinput-device.hpp
@@ -12,18 +12,18 @@ public:
 	int fd;
 
 	std::string name;
-	u16 vendor;
-	u16 product;
-	u32 version;
+	u16 vendor = 0;
+	u16 product = 0;
+	u32 version = 0;
 
-	UinputDevice(void);
-	~UinputDevice(void);
+	UinputDevice();
+	~UinputDevice();
 
 	void set_evbit(i32 ev);
 	void set_propbit(i32 prop);
 	void set_keybit(i32 key);
 	void set_absinfo(u16 code, i32 min, i32 max, i32 res);
-	void create(void);
+	void create();
 	void emit(u16 type, u16 key, i32 value);
 };
 
