c003d33b23b0ab5c1f9768a85dc41ed3a1af1be9
Dorian Stoll <dorian.stoll@tmsp.io>
2021-03-09 08:43:34 +0100

    debug: Port proto-rt tool from qzed
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

---
+++ src/debug/gfx/gtk.hpp
@@ -0,0 +1,482 @@
+#pragma once
+
+#include "gdk.hpp"
+
+#include <contacts/types.hpp>
+
+#include <functional>
+#include <gtk/gtk.h>
+#include <utility>
+
+namespace iptsd::gfx::gtk {
+
+namespace detail {
+
+template <class T> struct raw_type {
+	using type = typename T::raw_t;
+
+	static inline auto wrap(typename T::raw_t raw) -> T
+	{
+		return T::wrap_raw(raw);
+	}
+
+	static inline auto unwrap(T wrapped) -> typename T::raw_t
+	{
+		return wrapped.raw();
+	}
+};
+
+template <> struct raw_type<void> {
+	using type = void;
+};
+
+template <> struct raw_type<bool> {
+	using type = gboolean;
+
+	static inline auto wrap(gboolean raw) -> bool
+	{
+		return raw;
+	}
+
+	static inline auto unwrap(bool raw) -> gboolean
+	{
+		return raw;
+	}
+};
+
+template <class T> using raw_t = typename raw_type<T>::type;
+
+template <class T> inline auto wrap(raw_t<T> raw) -> T
+{
+	return raw_type<T>::wrap(raw);
+}
+
+template <class T> inline auto unwrap(T wrapped) -> raw_t<T>
+{
+	return raw_type<T>::unwrap(wrapped);
+}
+
+namespace signal {
+
+template <class Ret, class... Args>
+inline auto do_call_wrapped(std::function<Ret(Args...)> &fn, raw_t<Args>... args) -> raw_t<Ret>
+{
+	if constexpr (std::is_same_v<Ret, void>) {
+		fn(wrap<Args>(std::forward<raw_t<Args>>(args))...);
+	} else {
+		return unwrap(fn(wrap<Args>(std::forward<raw_t<Args>>(args))...));
+	}
+
+	// TODO: exception handling
+}
+
+template <class Ret, class... Args>
+inline auto call_wrapped(raw_t<Args>... args, gpointer data) -> raw_t<Ret>
+{
+	auto *fn = static_cast<std::function<Ret(Args...)> *>(data);
+
+	return do_call_wrapped(*fn, std::forward<raw_t<Args>>(args)...);
+}
+
+template <class Ret, class... Args> inline void destroy_wrapped(gpointer data, GClosure *closure)
+{
+	auto *fn = static_cast<std::function<Ret(Args...)> *>(data);
+	auto _destroy = std::unique_ptr<std::function<Ret(Args...)>>(fn);
+}
+
+template <class Ret, class... Args>
+inline auto do_connect(gpointer instance, char const *signal,
+		       std::function<Ret(Args...)> &&callback)
+{
+	auto cb = std::make_unique<std::function<Ret(Args...)>>(
+		std::forward<std::function<Ret(Args...)>>(callback));
+	auto *cw = &call_wrapped<Ret, Args...>;
+	auto flags = GConnectFlags(0);
+
+	return g_signal_connect_data(instance, signal, G_CALLBACK(cw), cb.release(),
+				     destroy_wrapped<Ret, Args...>, flags);
+}
+
+} /* namespace signal */
+} /* namespace detail */
+
+enum class ApplicationFlags {
+	None = G_APPLICATION_FLAGS_NONE,
+};
+
+enum class WindowPosition {
+	Center = GTK_WIN_POS_CENTER,
+};
+
+class Application {
+public:
+	using raw_t = GtkApplication *;
+
+public:
+	static auto create(char const *id, ApplicationFlags flags = ApplicationFlags::None)
+		-> Application;
+	static auto wrap_raw(gpointer raw) -> Application;
+
+public:
+	Application(GtkApplication *raw);
+	Application(Application const &other);
+	Application(Application &&other);
+	~Application();
+
+	auto operator=(Application const &rhs) -> Application &;
+	auto operator=(Application &&rhs) -> Application &;
+
+	auto raw() -> raw_t;
+	auto operator*() -> raw_t;
+
+	auto run(int argc, char **argv) -> int;
+
+	template <class F> auto connect(char const *signal, F callback) -> ulong;
+
+	// TODO: ...
+
+private:
+	GtkApplication *m_raw;
+};
+
+class Widget {
+public:
+	using raw_t = GtkWidget *;
+
+public:
+	static auto wrap_raw(gpointer raw) -> Widget;
+
+public:
+	Widget(GtkWidget *raw);
+	Widget(Widget const &other);
+	Widget(Widget &&other);
+	virtual ~Widget();
+
+	explicit operator bool() const;
+
+	auto operator=(Widget const &rhs) -> Widget &;
+	auto operator=(Widget &&rhs) -> Widget &;
+
+	auto raw() -> raw_t;
+	auto operator*() -> raw_t;
+
+	void show();
+	void show_all();
+
+	auto get_allocated_width() -> int;
+	auto get_allocated_height() -> int;
+
+	void queue_draw();
+
+	template <class F> auto connect(char const *signal, F callback) -> ulong;
+
+	// TODO: ...
+
+protected:
+	GtkWidget *m_raw;
+};
+
+class Container : public Widget {
+public:
+	using raw_t = GtkWidget *;
+
+public:
+	static auto wrap_raw(gpointer raw) -> Container;
+
+public:
+	Container(GtkContainer *raw);
+	Container(GtkWidget *raw);
+
+	void add(Widget &widget);
+};
+
+class Bin : public Container {
+public:
+	using raw_t = GtkWidget *;
+
+public:
+	static auto wrap_raw(gpointer raw) -> Bin;
+
+public:
+	Bin(GtkBin *raw);
+	Bin(GtkWidget *raw);
+};
+
+class Window : public Bin {
+public:
+	using raw_t = GtkWidget *;
+
+public:
+	static auto wrap_raw(gpointer raw) -> Window;
+
+public:
+	Window(GtkWindow *raw);
+	Window(GtkWidget *raw);
+
+	void set_position(WindowPosition position);
+	void set_title(char const *title);
+	void set_default_size(int width, int height);
+	void set_geometry_hints(gdk::Geometry const &geom, gdk::WindowHints geom_mask);
+};
+
+class ApplicationWindow : public Window {
+public:
+	using raw_t = GtkWidget *;
+
+public:
+	static auto create(Application &app) -> ApplicationWindow;
+	static auto wrap_raw(gpointer raw) -> ApplicationWindow;
+
+public:
+	ApplicationWindow(GtkApplicationWindow *raw);
+	ApplicationWindow(GtkWidget *raw);
+};
+
+class DrawingArea : public Widget {
+public:
+	using raw_t = GtkWidget *;
+
+public:
+	static auto create() -> DrawingArea;
+	static auto wrap_raw(gpointer raw) -> DrawingArea;
+
+public:
+	DrawingArea(GtkDrawingArea *raw);
+	DrawingArea(GtkWidget *raw);
+};
+
+inline auto Application::create(char const *id, ApplicationFlags flags) -> Application
+{
+	return {gtk_application_new(id, static_cast<GApplicationFlags>(flags))};
+}
+
+inline auto Application::wrap_raw(gpointer raw) -> Application
+{
+	return {GTK_APPLICATION(g_object_ref(raw))};
+}
+
+inline Application::Application(GtkApplication *raw) : m_raw {raw}
+{}
+
+inline Application::Application(Application const &other)
+	: m_raw {GTK_APPLICATION(g_object_ref(other.m_raw))}
+{}
+
+inline Application::Application(Application &&other) : m_raw {std::exchange(other.m_raw, nullptr)}
+{}
+
+inline Application::~Application()
+{
+	if (m_raw) {
+		g_object_unref(m_raw);
+	}
+}
+
+inline auto Application::operator=(Application const &rhs) -> Application &
+{
+	auto _destroy = Application(std::exchange(m_raw, nullptr));
+
+	m_raw = GTK_APPLICATION(g_object_ref(rhs.m_raw));
+	return *this;
+}
+
+inline auto Application::operator=(Application &&rhs) -> Application &
+{
+	m_raw = std::exchange(rhs.m_raw, nullptr);
+	return *this;
+}
+
+inline auto Application::raw() -> raw_t
+{
+	return m_raw;
+}
+
+inline auto Application::operator*() -> raw_t
+{
+	return m_raw;
+}
+
+inline auto Application::run(int argc, char **argv) -> int
+{
+	return g_application_run(G_APPLICATION(m_raw), argc, argv);
+}
+
+template <class F> inline auto Application::connect(char const *signal, F callback) -> ulong
+{
+	return detail::signal::do_connect(m_raw, signal, std::function {callback});
+}
+
+inline auto Widget::wrap_raw(gpointer raw) -> Widget
+{
+	return {GTK_WIDGET(g_object_ref(raw))};
+}
+
+inline Widget::Widget(GtkWidget *raw) : m_raw {raw}
+{}
+
+inline Widget::Widget(Widget const &other) : m_raw {GTK_WIDGET(g_object_ref(other.m_raw))}
+{}
+
+inline Widget::Widget(Widget &&other) : m_raw {std::exchange(other.m_raw, nullptr)}
+{}
+
+inline Widget::~Widget()
+{
+	if (m_raw) {
+		g_object_unref(m_raw);
+	}
+}
+
+Widget::operator bool() const
+{
+	return m_raw != nullptr;
+}
+
+auto Widget::operator=(Widget const &rhs) -> Widget &
+{
+	auto _destroy = Widget(std::exchange(m_raw, nullptr));
+
+	m_raw = GTK_WIDGET(g_object_ref(rhs.m_raw));
+	return *this;
+}
+
+auto Widget::operator=(Widget &&rhs) -> Widget &
+{
+	m_raw = std::exchange(rhs.m_raw, nullptr);
+	return *this;
+}
+
+inline auto Widget::raw() -> raw_t
+{
+	return m_raw;
+}
+
+inline auto Widget::operator*() -> raw_t
+{
+	return m_raw;
+}
+
+inline void Widget::show()
+{
+	gtk_widget_show(m_raw);
+}
+
+inline void Widget::show_all()
+{
+	gtk_widget_show_all(m_raw);
+}
+
+inline auto Widget::get_allocated_width() -> int
+{
+	return gtk_widget_get_allocated_width(m_raw);
+}
+
+inline auto Widget::get_allocated_height() -> int
+{
+	return gtk_widget_get_allocated_height(m_raw);
+}
+
+inline void Widget::queue_draw()
+{
+	gtk_widget_queue_draw(m_raw);
+}
+
+template <class F> inline auto Widget::connect(char const *signal, F callback) -> ulong
+{
+	return detail::signal::do_connect(m_raw, signal, std::function {callback});
+}
+
+inline auto Container::wrap_raw(gpointer raw) -> Container
+{
+	return {GTK_CONTAINER(g_object_ref(raw))};
+}
+
+inline Container::Container(GtkContainer *raw) : Widget {GTK_WIDGET(raw)}
+{}
+
+inline Container::Container(GtkWidget *raw) : Widget {raw}
+{}
+
+inline void Container::add(Widget &widget)
+{
+	gtk_container_add(GTK_CONTAINER(m_raw), *widget);
+}
+
+inline auto Bin::wrap_raw(gpointer raw) -> Bin
+{
+	return {GTK_BIN(g_object_ref(raw))};
+}
+
+inline Bin::Bin(GtkBin *raw) : Container {GTK_WIDGET(raw)}
+{}
+
+inline Bin::Bin(GtkWidget *raw) : Container {raw}
+{}
+
+inline auto Window::wrap_raw(gpointer raw) -> Window
+{
+	return {GTK_WINDOW(g_object_ref(raw))};
+}
+
+inline Window::Window(GtkWindow *raw) : Bin {GTK_WIDGET(raw)}
+{}
+
+inline Window::Window(GtkWidget *raw) : Bin {raw}
+{}
+
+inline void Window::set_position(WindowPosition position)
+{
+	gtk_window_set_position(GTK_WINDOW(m_raw), static_cast<GtkWindowPosition>(position));
+}
+
+inline void Window::set_title(char const *title)
+{
+	gtk_window_set_title(GTK_WINDOW(m_raw), title);
+}
+
+inline void Window::set_default_size(int width, int height)
+{
+	gtk_window_set_default_size(GTK_WINDOW(m_raw), width, height);
+}
+
+inline void Window::set_geometry_hints(gdk::Geometry const &geom, gdk::WindowHints geom_mask)
+{
+	auto geom_c = geom.c_struct();
+	auto mask = static_cast<GdkWindowHints>(geom_mask);
+
+	gtk_window_set_geometry_hints(GTK_WINDOW(m_raw), NULL, &geom_c, mask);
+}
+
+inline auto ApplicationWindow::wrap_raw(gpointer raw) -> ApplicationWindow
+{
+	return {GTK_APPLICATION_WINDOW(g_object_ref(raw))};
+}
+
+inline auto ApplicationWindow::create(Application &app) -> ApplicationWindow
+{
+	return wrap_raw(gtk_application_window_new(*app));
+}
+
+inline ApplicationWindow::ApplicationWindow(GtkApplicationWindow *raw) : Window {GTK_WIDGET(raw)}
+{}
+
+inline ApplicationWindow::ApplicationWindow(GtkWidget *raw) : Window {raw}
+{}
+
+inline auto DrawingArea::wrap_raw(gpointer raw) -> DrawingArea
+{
+	return {GTK_DRAWING_AREA(g_object_ref(raw))};
+}
+
+inline auto DrawingArea::create() -> DrawingArea
+{
+	return wrap_raw(gtk_drawing_area_new());
+}
+
+inline DrawingArea::DrawingArea(GtkDrawingArea *raw) : Widget {GTK_WIDGET(raw)}
+{}
+
+inline DrawingArea::DrawingArea(GtkWidget *raw) : Widget {raw}
+{}
+
+} /* namespace iptsd::gfx::gtk */
---
+++ src/debug/proto-rt.cpp
@@ -0,0 +1,177 @@
+#include "gfx/cairo.hpp"
+#include "gfx/gtk.hpp"
+#include "visualization.hpp"
+
+#include <contacts/container/image.hpp>
+#include <contacts/eval/perf.hpp>
+#include <contacts/processor.hpp>
+#include <contacts/types.hpp>
+#include <ipts/control.hpp>
+#include <ipts/parser.hpp>
+
+#include <atomic>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <spdlog/spdlog.h>
+#include <thread>
+#include <vector>
+
+using namespace iptsd::gfx;
+
+class MainContext {
+public:
+	MainContext(iptsd::index2_t img_size);
+
+	void submit(iptsd::container::Image<f32> const &img,
+		    std::vector<iptsd::TouchPoint> const &tps);
+
+	auto draw_event(cairo::Cairo &cr) -> bool;
+
+public:
+	gtk::Widget m_widget;
+
+private:
+	iptsd::Visualization m_vis;
+
+	iptsd::container::Image<f32> m_img1;
+	iptsd::container::Image<f32> m_img2;
+
+	std::vector<iptsd::TouchPoint> m_tps1;
+	std::vector<iptsd::TouchPoint> m_tps2;
+
+	std::mutex m_lock;
+
+	iptsd::container::Image<f32> *m_img_frnt;
+	iptsd::container::Image<f32> *m_img_back;
+
+	std::vector<iptsd::TouchPoint> *m_tps_frnt;
+	std::vector<iptsd::TouchPoint> *m_tps_back;
+	bool m_swap = false;
+};
+
+MainContext::MainContext(iptsd::index2_t img_size)
+	: m_widget {nullptr}, m_vis {img_size}, m_img1 {img_size}, m_img2 {img_size}, m_tps1 {},
+	  m_tps2 {}, m_img_frnt {&m_img1}, m_img_back {&m_img2}, m_tps_frnt {&m_tps1},
+	  m_tps_back {&m_tps2}
+{}
+
+void MainContext::submit(iptsd::Image<f32> const &img, std::vector<iptsd::TouchPoint> const &tps)
+{
+	{
+		// set swap to false to prevent read-access in draw
+		auto guard = std::lock_guard(m_lock);
+		m_swap = false;
+	}
+
+	// copy to back-buffer
+	*m_img_back = img;
+	*m_tps_back = tps;
+
+	{
+		// set swap to true to indicate that new data has arrived
+		auto guard = std::lock_guard(m_lock);
+		m_swap = true;
+	}
+
+	// request update
+	if (m_widget)
+		m_widget.queue_draw();
+}
+
+auto MainContext::draw_event(cairo::Cairo &cr) -> bool
+{
+	auto const width = m_widget.get_allocated_width();
+	auto const height = m_widget.get_allocated_height();
+
+	{
+		// check and swap buffers, if necessary
+		auto guard = std::lock_guard(m_lock);
+
+		if (m_swap) {
+			std::swap(m_img_frnt, m_img_back);
+			std::swap(m_tps_frnt, m_tps_back);
+			m_swap = false;
+		}
+	}
+
+	m_vis.draw(cr, *m_img_frnt, *m_tps_frnt, width, height);
+	return false;
+}
+
+int main(int argc, char *argv[])
+{
+	spdlog::set_pattern("[%X.%e] [%^%l%$] %v");
+
+	const iptsd::index2_t size {72, 48};
+	MainContext ctx {size};
+	iptsd::TouchProcessor prc {size};
+
+	IptsControl ctrl;
+
+	auto app = gtk::Application::create("com.github.qzed.digitizer-prototype.rt");
+
+	app.connect("activate", [&](gtk::Application app) -> void {
+		auto window = gtk::ApplicationWindow::create(app);
+
+		// fix aspect to 3-to-2
+		auto geom =
+			gdk::Geometry {0, 0, 0, 0, 0, 0, 0, 0, 1.5f, 1.5f, gdk::Gravity::Center};
+
+		window.set_position(gtk::WindowPosition::Center);
+		window.set_default_size(900, 600);
+		window.set_title("IPTS Processor Prototype");
+		window.set_geometry_hints(geom, gdk::WindowHints::Aspect);
+
+		auto darea = gtk::DrawingArea::create();
+		window.add(darea);
+
+		ctx.m_widget = darea;
+		darea.connect("draw", [&](gtk::Widget widget, cairo::Cairo cr) -> bool {
+			return ctx.draw_event(cr);
+		});
+
+		window.show_all();
+	});
+
+	std::atomic_bool run(true);
+
+	std::thread updt([&]() -> void {
+		using namespace std::chrono_literals;
+
+		IptsParser parser(ctrl.info.buffer_size);
+		parser.on_heatmap = [&](const auto &data) {
+			iptsd::container::Image<f32> hm {size};
+
+			std::transform(data.data.begin(), data.data.end(), hm.begin(), [&](auto v) {
+				f32 val = static_cast<f32>(v - data.z_min) /
+					  static_cast<f32>(data.z_max - data.z_min);
+
+				return 1.0f - val;
+			});
+
+			ctx.submit(hm, prc.process(hm));
+		};
+
+		while (run.load()) {
+			u32 doorbell = ctrl.doorbell();
+
+			while (doorbell > ctrl.current_doorbell && run.load()) {
+				ctrl.read(parser.buffer());
+				parser.parse();
+				ctrl.send_feedback();
+			}
+
+			std::this_thread::sleep_for(10ms);
+		}
+	});
+
+	int status = app.run(argc, argv);
+
+	// TODO: should probably hook into destroy event to stop thread before gtk_main() returns
+
+	run.store(false);
+	updt.join();
+
+	return status;
+}
