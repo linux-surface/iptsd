3b4f6811847a908d1bd57765c0ed72c221c56a57
Dorian Stoll <dorian.stoll@tmsp.io>
2021-03-13 19:57:19 +0100

    common: Disable clang-tidy check for likely/unlikely macros
    Yes these look like functions, and that is discouraged, but in this case it is
    what we actually want.
    
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- src/common/compiler.hpp
+++ src/common/compiler.hpp
@@ -3,7 +3,7 @@
 #ifndef IPTSD_COMMON_COMPILER_HPP
 #define IPTSD_COMMON_COMPILER_HPP
 
-#define likely(X)   __builtin_expect(!!(X), 1)
-#define unlikely(X) __builtin_expect(!!(X), 0)
+#define likely(X)   __builtin_expect(!!(X), 1) /* NOLINT(cppcoreguidelines-macro-usage) */
+#define unlikely(X) __builtin_expect(!!(X), 0) /* NOLINT(cppcoreguidelines-macro-usage) */
 
 #endif /* IPTSD_COMMON_COMPILER_HPP */
