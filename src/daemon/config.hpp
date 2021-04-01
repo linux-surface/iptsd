a9896e3331d1c2acbc6f61b93f1f9c84e3c0a77a
Dorian Stoll <dorian.stoll@tmsp.io>
2021-04-01 12:02:25 +0200

    daemon: Let DisableTouchOnStylus default to true for now
    The palm rejection while writing with the stylus is extremely jittery for me, so
    lets enable this by default until palm rejection has been fine tuned.
    
    Signed-off-by: Dorian Stoll <dorian.stoll@tmsp.io>

--- src/daemon/config.hpp
+++ src/daemon/config.hpp
@@ -18,7 +18,8 @@ public:
 	i32 width = 0;
 	i32 height = 0;
 
-	bool disable_touch_on_stylus = false;
+	// TODO: Default to false, when palm rejection has been tuned
+	bool disable_touch_on_stylus = true;
 
 	struct ipts_device_info info;
 
