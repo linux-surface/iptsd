8d2ef04f4f4419b978047b89e36a3f5040de031b
Dorian Stoll <dorian.stoll@tmsp.io>
2023-03-14 19:28:05 +0100

    hid: Move implementations into header files and add docs

--- src/hid/descriptor.hpp
+++ src/hid/descriptor.hpp
@@ -1,32 +1,238 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef IPTSD_HID_DESCRIPTOR_HPP
 #define IPTSD_HID_DESCRIPTOR_HPP
 
-#include <common/types.hpp>
-#include <hid/shim/hidrd.h>
+#include "shim/hidrd.h"
 
-#include <cstddef>
+#include <common/types.hpp>
+
 #include <gsl/gsl>
 #include <vector>
 
 namespace iptsd::hid {
 
 class Descriptor {
-public:
-	void load(gsl::span<u8> raw);
-	[[nodiscard]] const std::vector<const hidrd_item *> &items() const;
-
-	[[nodiscard]] std::vector<u8> reports(hidrd_item_main_tag type) const;
-	[[nodiscard]] std::vector<hidrd_usage> usage(u8 report) const;
-	[[nodiscard]] hidrd_usage_page usage_page(u8 report) const;
-	[[nodiscard]] u64 size(u8 report) const;
-
 private:
-	std::vector<u8> descriptor {};
-	std::vector<const hidrd_item *> parsed {};
+	std::vector<u8> m_descriptor {};
+	std::vector<const hidrd_item *> m_parsed {};
+
+public:
+	/*!
+	 * Loads a binary HID descriptor and parses it.
+	 *
+	 * @param[in] raw The raw binary data to load and parse.
+	 */
+	void load(gsl::span<u8> raw)
+	{
+		usize size = 0;
+
+		m_descriptor.clear();
+		m_descriptor.resize(raw.size());
+
+		std::copy(raw.begin(), raw.end(), m_descriptor.begin());
+
+		while (size < m_descriptor.size()) {
+			// NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+			auto item = reinterpret_cast<const hidrd_item *>(&m_descriptor[size]);
+			size += hidrd_item_get_size(item);
+
+			m_parsed.push_back(item);
+		}
+	}
+
+	/*!
+	 * All hidrd_items of the HID report.
+	 *
+	 * Every item corresponds to one tag of the HID descriptor.
+
+	 * @return A list of all items found in the HID descriptor.
+	 */
+	[[nodiscard]] const std::vector<const hidrd_item *> &items() const
+	{
+		return m_parsed;
+	}
+
+	/*!
+	 * Returns all reports matching a certain type.
+	 *
+	 * This can be used to get a list of all Input or Feature reports.
+	 *
+	 * @param[in] type The type of report to search for.
+	 * @return A list of all report IDs of the given type.
+	 */
+	[[nodiscard]] std::vector<u8> reports(hidrd_item_main_tag type) const
+	{
+		u8 current = 0;
+		std::vector<u8> reports {};
+
+		for (const hidrd_item *item : this->items()) {
+			if (this->is_report(item)) {
+				current = hidrd_item_report_id_get_value(item);
+				continue;
+			}
+
+			if (!this->is_commit(item))
+				continue;
+
+			if (current == 0)
+				continue;
+
+			if (hidrd_item_main_get_tag(item) == type) {
+				reports.push_back(current);
+				current = 0;
+			}
+		}
+
+		return reports;
+	}
+
+	/*!
+	 * Returns the usage tags of a HID report.
+	 *
+	 * @param[in] report The report ID for which to get the usage tags.
+	 * @return The values of all usage tags that are active for the report.
+	 */
+	[[nodiscard]] std::vector<hidrd_usage> usage(u8 report) const
+	{
+		u8 current = 0;
+		std::vector<hidrd_usage> usage {};
+
+		for (const hidrd_item *item : this->items()) {
+			if (this->is_report(item)) {
+				current = hidrd_item_report_id_get_value(item);
+				continue;
+			}
+
+			if (current != report)
+				continue;
+
+			if (hidrd_item_short_get_type(item) != HIDRD_ITEM_SHORT_TYPE_LOCAL)
+				continue;
+
+			if (hidrd_item_local_get_tag(item) != HIDRD_ITEM_LOCAL_TAG_USAGE)
+				continue;
+
+			usage.push_back(hidrd_item_usage_get_value(item));
+		}
+
+		return usage;
+	}
+
+	/*!
+	 * The usage page of a HID report.
+	 *
+	 * @param[in] report The report ID for which to get the usage page.
+	 * @return The value of the usage page tag that is active for the report.
+	 */
+	[[nodiscard]] hidrd_usage_page usage_page(u8 report) const
+	{
+		hidrd_usage_page current = HIDRD_USAGE_PAGE_MAX;
+
+		for (const hidrd_item *item : this->items()) {
+			if (this->is_report(item) && report == hidrd_item_report_id_get_value(item))
+				return current;
+
+			if (hidrd_item_short_get_type(item) != HIDRD_ITEM_SHORT_TYPE_GLOBAL)
+				continue;
+
+			if (hidrd_item_global_get_tag(item) != HIDRD_ITEM_GLOBAL_TAG_USAGE_PAGE)
+				continue;
+
+			current = hidrd_item_usage_page_get_value(item);
+		}
+
+		return HIDRD_USAGE_PAGE_MAX;
+	}
+
+	/*!
+	 * Calculates the size of a HID report.
+	 *
+	 * @param[in] report The report ID for which to calculate the size.
+	 * @return The combined size of the HID report in bytes.
+	 */
+	[[nodiscard]] u64 size(u8 report) const
+	{
+		u8 current = 0;
+
+		u32 report_count = 0;
+		u32 report_size = 0;
+		u64 total_size = 0;
+
+		for (const hidrd_item *item : this->items()) {
+			if (is_report(item)) {
+				current = hidrd_item_report_id_get_value(item);
+				continue;
+			}
+
+			if (current != report)
+				continue;
+
+			if (is_commit(item)) {
+				total_size += (report_count * report_size) / 8;
+				continue;
+			}
+
+			if (hidrd_item_short_get_type(item) != HIDRD_ITEM_SHORT_TYPE_GLOBAL)
+				continue;
+
+			if (hidrd_item_global_get_tag(item) == HIDRD_ITEM_GLOBAL_TAG_REPORT_COUNT) {
+				report_count = hidrd_item_report_count_get_value(item);
+				continue;
+			}
+
+			if (hidrd_item_global_get_tag(item) == HIDRD_ITEM_GLOBAL_TAG_REPORT_SIZE) {
+				report_size = hidrd_item_report_size_get_value(item);
+				continue;
+			}
+		}
+
+		return total_size;
+	}
+
+	/*!
+	 * Whether an item represents a Report ID tag.
+	 *
+	 * @param[in] item The item to check.
+	 * @return true if the item is the definition of a Report ID.
+	 */
+	[[nodiscard]] bool is_report(const hidrd_item *item) const
+	{
+		if (hidrd_item_basic_get_format(item) != HIDRD_ITEM_BASIC_FORMAT_SHORT)
+			return false;
+
+		if (hidrd_item_short_get_type(item) != HIDRD_ITEM_SHORT_TYPE_GLOBAL)
+			return false;
+
+		return hidrd_item_global_get_tag(item) == HIDRD_ITEM_GLOBAL_TAG_REPORT_ID;
+	}
+
+	/*!
+	 * Whether a hidrd item "commits" a report.
+	 *
+	 * With the state machine that HID uses, an Input, Output or Feature tag
+	 * will create a new report with the current state, e.g. everything that preceded
+	 * it.
+	 *
+	 * This checks whether an item represents one of these "committing" tags.
+	 *
+	 * @param[in] item The item to check.
+	 * @return Whether the item is a "committing" tag.
+	 */
+	[[nodiscard]] bool is_commit(const hidrd_item *item) const
+	{
+		if (hidrd_item_basic_get_format(item) != HIDRD_ITEM_BASIC_FORMAT_SHORT)
+			return false;
+
+		if (hidrd_item_short_get_type(item) != HIDRD_ITEM_SHORT_TYPE_MAIN)
+			return false;
+
+		return hidrd_item_main_get_tag(item) == HIDRD_ITEM_MAIN_TAG_INPUT ||
+		       hidrd_item_main_get_tag(item) == HIDRD_ITEM_MAIN_TAG_OUTPUT ||
+		       hidrd_item_main_get_tag(item) == HIDRD_ITEM_MAIN_TAG_FEATURE;
+	}
 };
 
-} /* namespace iptsd::hid */
+} // namespace iptsd::hid
 
-#endif /* IPTSD_HID_DESCRIPTOR_HPP */
+#endif // IPTSD_HID_DESCRIPTOR_HPP
--- src/hid/device.hpp
+++ src/hid/device.hpp
@@ -1,10 +1,12 @@
-/* SPDX-License-Identifier: GPL-2.0-or-later */
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #ifndef IPTSD_HID_DEVICE_HPP
 #define IPTSD_HID_DEVICE_HPP
 
 #include "descriptor.hpp"
 
+#include <common/cerror.hpp>
+#include <common/cwrap.hpp>
 #include <common/types.hpp>
 
 #include <gsl/gsl>
@@ -15,24 +17,112 @@
 namespace iptsd::hid {
 
 class Device {
-public:
-	Device(const std::string &path);
-
-	[[nodiscard]] i16 product() const;
-	[[nodiscard]] i16 vendor() const;
-
-	[[nodiscard]] const Descriptor &descriptor() const;
-
-	[[nodiscard]] ssize_t read(gsl::span<u8> buffer) const;
-	void get_feature(gsl::span<u8> report) const;
-	void set_feature(gsl::span<u8> report) const;
-
 private:
-	int device = -1;
-	struct hidraw_devinfo devinfo {};
-	Descriptor desc {};
+	int m_fd = -1;
+	struct hidraw_devinfo m_devinfo {};
+
+	// The HID descriptor of the device.
+	Descriptor m_desc {};
+
+public:
+	Device(const std::string &path)
+	{
+		int ret = common::open(path, O_RDWR);
+		if (ret == -1)
+			throw common::cerror("Failed to open hidraw device");
+
+		m_fd = ret;
+
+		ret = common::ioctl(m_fd, HIDIOCGRAWINFO, &m_devinfo);
+		if (ret == -1)
+			throw common::cerror("Failed to read HID device info");
+
+		u32 desc_size = 0;
+
+		ret = common::ioctl(m_fd, HIDIOCGRDESCSIZE, &desc_size);
+		if (ret == -1)
+			throw common::cerror("Failed to read HID descriptor size");
+
+		struct hidraw_report_descriptor hidraw_desc {};
+		hidraw_desc.size = desc_size;
+
+		ret = common::ioctl(m_fd, HIDIOCGRDESC, &hidraw_desc);
+		if (ret == -1)
+			throw common::cerror("Failed to read HID descriptor");
+
+		m_desc.load(gsl::span<u8> {&hidraw_desc.value[0], desc_size});
+	}
+
+	/*!
+	 * The vendor ID of the HID device.
+	 *
+	 * @return The vendor ID of the HID device.
+	 */
+	[[nodiscard]] i16 product() const
+	{
+		return m_devinfo.product;
+	}
+
+	/*!
+	 * The product ID of the HID device.
+	 *
+	 * @return The product ID of the HID device.
+	 */
+	[[nodiscard]] i16 vendor() const
+	{
+		return m_devinfo.vendor;
+	}
+
+	/*!
+	 * The HID descriptor of the device.
+	 *
+	 * @return A reference to the wrapped HID descriptor of the device.
+	 */
+	[[nodiscard]] const Descriptor &descriptor() const
+	{
+		return m_desc;
+	}
+
+	/*!
+	 * Reads a report from the HID device.
+	 *
+	 * @param[in] buffer The target storage for the report.
+	 * @return The size of the report that was read in bytes.
+	 */
+	[[nodiscard]] isize read(gsl::span<u8> buffer) const
+	{
+		const isize ret = common::read(m_fd, buffer);
+		if (ret == -1)
+			throw common::cerror("Failed to read from HID device");
+
+		return ret;
+	}
+
+	/*!
+	 * Gets the data of a HID feature report.
+	 *
+	 * @param[in] report The report ID to get, followed by enough space to fit the data.
+	 */
+	void get_feature(gsl::span<u8> report) const
+	{
+		const int ret = common::ioctl(m_fd, HIDIOCGFEATURE(report.size()), report.data());
+		if (ret == -1)
+			throw common::cerror("Failed to get feature");
+	}
+
+	/*!
+	 * Sets the data of a HID feature report.
+	 *
+	 * @param[in] report The report ID to set, followed by the new data.
+	 */
+	void set_feature(gsl::span<u8> report) const
+	{
+		const int ret = common::ioctl(m_fd, HIDIOCSFEATURE(report.size()), report.data());
+		if (ret == -1)
+			throw common::cerror("Failed to set feature");
+	}
 };
 
 } // namespace iptsd::hid
 
-#endif /* IPTSD_HID_DEVICE_HPP */
+#endif // IPTSD_HID_DEVICE_HPP
