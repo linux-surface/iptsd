b0827412316eeb27d5776880a959089aa5d9d1e0
Dorian Stoll <dorian.stoll@tmsp.io>
2023-04-28 18:32:46 +0200

    common: Correct definition of f32 and f64 types
    The C / C++ standard guarantees float to be 32 bit and double to be 64 bit.
    float_t and double_t can have their size modified via a preprocessor directive.

--- src/common/types.hpp
+++ src/common/types.hpp
@@ -24,8 +24,8 @@ using u16 = std::uint16_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
 
-using f32 = std::float_t;
-using f64 = std::double_t;
+using f32 = float;
+using f64 = double;
 
 using isize = std::ptrdiff_t;
 using usize = std::size_t;
