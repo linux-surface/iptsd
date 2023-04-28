0376f636d928ea1d2ed68c6d28eb012ed1e83289
Dorian Stoll <dorian.stoll@tmsp.io>
2023-04-28 22:34:15 +0200

    common: Add an assertion that prevents casting doubles to floats

--- src/common/casts.hpp
+++ src/common/casts.hpp
@@ -7,6 +7,8 @@
 #define EIGEN_DEFAULT_TO_ROW_MAJOR
 #endif
 
+#include "types.hpp"
+
 #include <Eigen/Eigen>
 #include <gsl/gsl>
 
@@ -17,6 +19,9 @@ namespace iptsd::casts {
 template <class To, class From>
 constexpr inline To to(const From value)
 {
+	static_assert(!std::is_same_v<From, f64> || !std::is_same_v<To, f32>,
+		      "f64 to f32 cannot be narrowed safely. Please use gsl::narrow_cast!");
+
 	using Common = std::common_type_t<To, From>;
 
 	// Can To contain From?
