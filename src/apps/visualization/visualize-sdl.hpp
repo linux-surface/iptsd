c97dffbb0b859c1e6699afee316e19f791687631
Ivor Wanders <ivor@iwanders.net>
2023-12-23 10:26:16 -0500

    apps: visualization: Handle window events properly
    Prevent the window from being marked as unresponding.

--- src/apps/visualization/visualize-sdl.hpp
+++ src/apps/visualization/visualize-sdl.hpp
@@ -68,6 +68,10 @@ public:
 	{
 		Visualize::on_data(data);
 
+		// Handle window events, such as the X11_NET_WM_PING
+		// event that is used to detect stuck programs.
+		SDL_PumpEvents();
+
 		constexpr usize FPS = 60;
 
 		// Limit how many times per seconds the screen is redrawn.
