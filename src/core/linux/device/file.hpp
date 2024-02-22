b14d2bc8ea85cfed03d99048d52742579135b2bc
Dorian Stoll <dorian.stoll@tmsp.io>
2024-02-22 13:33:56 +0100

    core: linux: Implement HID device that reads from a file

---
+++ src/core/linux/device/file.hpp
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#ifndef IPTSD_CORE_LINUX_DEVICE_FILE_HPP
+#define IPTSD_CORE_LINUX_DEVICE_FILE_HPP
+
+#include <common/casts.hpp>
+#include <common/file.hpp>
+#include <common/reader.hpp>
+#include <common/types.hpp>
+#include <hid/device.hpp>
+#include <hid/parser.hpp>
+#include <hid/report.hpp>
+#include <ipts/parser.hpp>
+
+#include <gsl/gsl>
+
+#include <linux/hidraw.h>
+
+#include <filesystem>
+#include <vector>
+
+namespace iptsd::core::linux::device {
+
+class File : public hid::Device {
+protected:
+	Reader m_data;
+	std::filesystem::path m_path {};
+
+	struct hidraw_devinfo m_devinfo {};
+	struct hidraw_report_descriptor m_desc {};
+
+	std::vector<hid::Report> m_reports {};
+
+public:
+	File(const std::filesystem::path &path)
+		: m_data {common::read_all_bytes(path)},
+		  m_path {path}
+	{
+		m_devinfo = m_data.read<struct hidraw_devinfo>();
+		m_desc.size = m_data.read<u32>();
+
+		const gsl::span<u8> desc {&m_desc.value[0], m_desc.size};
+		m_data.read(desc);
+
+		hid::parse(desc, m_reports);
+	}
+
+	/*!
+	 * The "name", aka. the path to the source file.
+	 */
+	std::string_view name() override
+	{
+		return m_path.c_str();
+	}
+
+	/*!
+	 * The vendor ID of the device.
+	 */
+	u16 vendor() override
+	{
+		/*
+		 * The value is just an ID stored in a signed value.
+		 * A negative device ID doesn't make sense, so cast it away.
+		 */
+		return gsl::narrow_cast<u16>(m_devinfo.vendor);
+	}
+
+	/*!
+	 * The product ID of the device.
+	 */
+	u16 product() override
+	{
+		/*
+		 * The value is just an ID stored in a signed value.
+		 * A negative device ID doesn't make sense, so cast it away.
+		 */
+		return gsl::narrow_cast<u16>(m_devinfo.product);
+	}
+
+	/*!
+	 * The HID descriptor of the device.
+	 */
+	const std::vector<hid::Report> &descriptor() override
+	{
+		return m_reports;
+	}
+
+	/*!
+	 * Reads a report from the stored HID data.
+	 *
+	 * @param[in] buffer The target storage for the report.
+	 * @return The size of the report that was read in bytes.
+	 */
+	usize read(gsl::span<u8> buffer) override
+	{
+		const auto size = casts::to<usize>(m_data.read<u64>());
+		m_data.read(buffer.first(size));
+		return size;
+	}
+
+	/*!
+	 * Reads the value of a HID feature report from the stored data.
+	 *
+	 * @param[in] report The report ID to get, followed by enough space to fit the data.
+	 */
+	void get_feature(gsl::span<u8> report) override
+	{
+		const auto size = casts::to<usize>(m_data.read<u64>());
+		m_data.read(report.first(size));
+	}
+
+	/*!
+	 * Sets the value of a HID feature report.
+	 *
+	 * This function is not implemented.
+	 *
+	 * Setting features is serialized implicitly, because the behaviour of the device and
+	 * therefor the following data will change.
+	 *
+	 * @param[in] report The report ID to set, followed by the new data.
+	 */
+	void set_feature(const gsl::span<u8> /* unused */) override
+	{
+	}
+};
+
+} // namespace iptsd::core::linux::device
+
+#endif // IPTSD_CORE_LINUX_DEVICE_FILE_HPP
