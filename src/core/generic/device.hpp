c1e3de3e6a9d77ee2621a550496557f86e1ecfd1
Dorian Stoll <dorian.stoll@tmsp.io>
2024-02-23 18:09:49 +0100

    core: Remove buffer_size and packing from device info

--- src/core/generic/device.hpp
+++ src/core/generic/device.hpp
@@ -10,18 +10,11 @@ namespace iptsd::core {
 /*
  * Contains informations about the device that produced the data
  * that is processed by an application.
- *
- * This struct is packed so that it can be part of binary dumps.
- * It is padded manually to keep compatibility with older files.
  */
-struct [[gnu::packed]] DeviceInfo {
+struct DeviceInfo {
 	u16 vendor;
 	u16 product;
-	u8 padding[4]; // NOLINT(cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
-	u64 buffer_size;
 };
-
-static_assert(sizeof(DeviceInfo) == 16);
 
 } // namespace iptsd::core
 
