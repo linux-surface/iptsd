4ed9f2f5930d7da240065657e5033c6311cbb4a5
Dorian Stoll <dorian.stoll@tmsp.io>
2024-04-20 21:21:47 +0200

    apps: daemon: Only create devices that are required
    We don't need a stylus device for touchpads. And disabling a device
    through the config now hides it instead of making it do nothing.

--- src/apps/daemon/daemon.hpp
+++ src/apps/daemon/daemon.hpp
@@ -21,57 +21,63 @@ namespace iptsd::apps::daemon {
 class Daemon : public core::Application {
 private:
 	// The touch device.
-	TouchDevice m_touch;
+	std::optional<TouchDevice> m_touch = std::nullopt;
 
 	// The stylus device.
-	StylusDevice m_stylus;
+	std::optional<StylusDevice> m_stylus = std::nullopt;
 
 public:
 	Daemon(const core::Config &config, const core::DeviceInfo &info)
-		: core::Application(config, info),
-		  m_touch {config, info},
-		  m_stylus {config, info} {};
+		: core::Application(config, info)
+	{
+		const bool create_touch =
+			(m_info.is_touchscreen() && !m_config.touchscreen_disable) ||
+			(m_info.is_touchpad() && !m_config.touchpad_disable);
+
+		if (create_touch)
+			m_touch.emplace(config, info);
+
+		if (m_info.is_touchscreen() && !m_config.stylus_disable)
+			m_stylus.emplace(config, info);
+	}
 
 	void on_start() override
 	{
-		if (m_config.touchpad_disable && m_info.is_touchpad())
-			spdlog::warn("Touchpad is disabled!");
-
-		if (m_config.touchscreen_disable && m_info.is_touchscreen())
+		if (!m_touch.has_value() && m_info.is_touchscreen())
 			spdlog::warn("Touchscreen is disabled!");
 
-		if (m_config.stylus_disable && m_info.is_touchpad())
+		if (!m_touch.has_value() && m_info.is_touchpad())
+			spdlog::warn("Touchpad is disabled!");
+
+		if (!m_stylus.has_value())
 			spdlog::warn("Stylus is disabled!");
 	}
 
 	void on_touch(const std::vector<contacts::Contact<f64>> &contacts) override
 	{
-		if (m_config.touchpad_disable && m_info.is_touchpad())
-			return;
-
-		if (m_config.touchscreen_disable && m_info.is_touchscreen())
+		if (!m_touch.has_value())
 			return;
 
 		// Enable the touchscreen if it was disabled by a stylus that is no longer active.
-		if (m_config.touchscreen_disable_on_stylus && m_info.is_touchscreen()) {
-			if (!m_stylus.active() && !m_touch.enabled())
-				m_touch.enable();
+		if (m_config.touchscreen_disable_on_stylus && m_stylus.has_value()) {
+			if (!m_stylus->active() && !m_touch->enabled())
+				m_touch->enable();
 		}
 
-		m_touch.update(contacts);
+		m_touch->update(contacts);
 	}
 
 	void on_stylus(const ipts::samples::Stylus &stylus) override
 	{
-		if (m_config.stylus_disable)
+		if (!m_stylus.has_value())
 			return;
 
-		if (m_config.touchscreen_disable_on_stylus && m_info.is_touchscreen()) {
-			if (m_touch.enabled())
-				m_touch.disable();
+		if (m_config.touchscreen_disable_on_stylus && m_touch.has_value()) {
+			if (m_touch->enabled())
+				m_touch->disable();
 		}
 
-		m_stylus.update(stylus);
+		m_stylus->update(stylus);
 	}
 };
 
