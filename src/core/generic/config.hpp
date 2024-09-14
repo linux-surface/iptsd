dd0c322b8ef48378d410b5d0e762212a7b680797
Dorian Stoll <dorian.stoll@tmsp.io>
2024-09-14 22:20:20 +0200

    core: generic: Adjust the contact detection defaults a bit

--- src/core/generic/config.hpp
+++ src/core/generic/config.hpp
@@ -38,14 +38,14 @@ public:
 	// [Contacts]
 	std::string contacts_neutral = "mode";
 	f64 contacts_neutral_value = 0;
-	f64 contacts_activation_threshold = 24;
-	f64 contacts_deactivation_threshold = 20;
+	f64 contacts_activation_threshold = 40;
+	f64 contacts_deactivation_threshold = 36;
 	f64 contacts_size_thresh_min = 0.1;
 	f64 contacts_size_thresh_max = 0.5;
 	f64 contacts_position_thresh_min = 0.04;
 	f64 contacts_position_thresh_max = 2;
 	f64 contacts_orientation_thresh_min = 1;
-	f64 contacts_orientation_thresh_max = 5;
+	f64 contacts_orientation_thresh_max = 15;
 	f64 contacts_size_min = 0.2;
 	f64 contacts_size_max = 2;
 	f64 contacts_aspect_min = 1;
